f3c057adf4ae2bbd9eaea32230314033e6ffca6d
SangwonJ1 <128347740+SangwonJ1@users.noreply.github.com>
2023-05-11 18:08:15 +0900

    Create sensordata_server.py

---
+++ sensordata_server.py
@@ -0,0 +1,46 @@
+import socket
+
+# Change the port number(포트 번호는 계속해서 수정할 것)
+PORT = 12355
+
+# Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+s.bind(('', PORT))
+
+# Listen for incoming connections
+s.listen(1)
+
+print('Server is running and waiting for connections...')
+
+# Set the thresholds for temperature, humidity, and CO level
+TEMP_THRESHOLD = 27
+HUMIDITY_THRESHOLD = 39
+CO_THRESHOLD = 36
+
+while True:
+    # Wait for a connection
+    conn, addr = s.accept()
+
+    print('Connected by', addr)
+    
+    # Receive the data in small chunks
+    data = conn.recv(1024)
+    
+    if data:
+        print('Received data: ', data.decode())
+        
+        # Parse the received data
+        data = data.decode().split(',')
+        temp = float(data[0].split(': ')[1][:-2])
+        humidity = float(data[1].split(': ')[1][:-1])
+        co = int(data[2].split(': ')[1])
+        sound = int(data[3].split(': ')[1])
+        
+        # Check if the thresholds are exceeded or sound is detected
+        if temp > TEMP_THRESHOLD and humidity > HUMIDITY_THRESHOLD and co > CO_THRESHOLD and sound == 1:
+            print("화재가 발생하였습니다.")
+
+    # Close the connection
+    conn.close()
